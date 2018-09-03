3affa24d8792df1c7b46c940f32546e098825a3c
Lucio <cuixiaolu@foxmail.com>
2018-09-03 23:16:55 +0800

    更新一部分内容

---
+++ TaskMall.Web/Controllers/AuthController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TaskMall.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        // GET: Auth
+        //登录页面
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
